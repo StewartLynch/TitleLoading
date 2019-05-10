ecaacc4f1c7a0e192601e81972a6fad7e7039a7a
Stewart Lynch <slynch@createchsol.com>
2019-05-10 15:00:31 -0700

    Initial Commit

---
+++ TitleLoading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TitleLoading
+//
+//  Created by Stewart Lynch on 2019-05-10.
+//  Copyright © 2019 Stewart Lynch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
